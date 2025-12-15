57ca8b3d65bccf5483aca51ddca6a3306f0d6890
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-12-15 22:35:42 +0000

    finished

--- Scoreboard.java
+++ Scoreboard.java
@@ -7,4 +7,49 @@
 
 */
 
-// Write the class below
+public class Scoreboard
+{
+    private String team1;
+    private String team2;
+    private String currentTeam;
+    private int score1 = 0;
+    private int score2 = 0;
+
+    public Scoreboard(String iTeam1, String iTeam2)
+    {
+        team1 = iTeam1;
+        team2 = iTeam2;
+        currentTeam = team1;
+    }
+
+    public void recordPlay(int points)
+    {
+        if (points > 0)
+        {
+            if (currentTeam.equals(team1))
+            {
+                score1 += points;
+            }
+            else 
+            {
+                score2 += points;
+            }
+        }
+        else 
+        {
+            if (currentTeam.equals(team1))
+            {
+                currentTeam = team2;
+            }
+            else 
+            {
+                currentTeam = team1;
+            }
+        }
+    }
+
+    public String getScore()
+    {
+        return (score1 + "-" + score2 + "-" + currentTeam);
+    }
+}
